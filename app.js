5d022c6e3bbcf660ed7bda4016102695326fd13e
Aksh-Ay007 <akshayjyothip@gmail.com>
2025-08-30 15:36:27 +0400

    ejs mjs module

---
+++ app.js
@@ -0,0 +1,9 @@
+// const { calcluate, c } = require("./xyz");
+
+// import {calcluate} from './xyz.js'
+
+const calcluate=require('./xyz')
+
+
+
+calcluate(1, 2);
---
+++ xyz.js
@@ -0,0 +1,23 @@
+let c = 10;
+let y = 12;
+
+let sum = c + y;
+
+// export function calcluate(a, b) {
+//   let sum = a + b;
+
+//   console.log(sum);
+// }
+
+
+function calculate(a,b){
+
+  let sum=a+b
+  console.log(sum);
+}
+
+module.exports=calculate
+
+
+
+// module.exports = { calcluate: calcluate, c: c }; common js module
