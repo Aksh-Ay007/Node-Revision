012aa44c6c9f56b01a0cf6497e4bf8e736845133
Aksh-Ay007 <akshayjyothip@gmail.com>
2025-09-01 20:11:13 +0400

    event loop

--- async.js
+++ async.js
@@ -1,19 +1,13 @@
-const { log } = require('console');
 const fs=require('fs')
-
 const https=require('https')
-
-console.log('hello world');
 
 var a=10
 var b=20;
-
 
 https.get("https://jsonplaceholder.typicode.com/users?utm_source=chatgpt.com",(res)=>{
 
   console.log('feteched data');
 });
-
 
 setTimeout(() => {
 
@@ -21,18 +15,16 @@ setTimeout(() => {
 }, 5000);
 
 
+fs.readFileSync('./note.txt','utf-8')
+
 fs.readFile("./note.txt", "utf-8", (err,data)=>{
   console.log('text read succes',data);
 })
 
-
 function multiply(a,b){
-
-
   let res=a*b
 return res
 }
-
 
 var c=multiply(a, b)
 
---
+++ eventloop.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+let a = 10;
+let b = 20;
+
+setImmediate(() => console.log("SetImmediate: Executed outside I/O events."));
+
+fs.readFile("./note.txt", "utf-8", (err, data) => {
+  process.nextTick(() => console.log("NextTick: Executed after I/O callback (readFile)."));
+
+  setTimeout(() => {
+    console.log("setTimeout: Executed after I/O callback (readFile).");
+  }, 0);
+
+  setImmediate(() => {
+    console.log("SetImmediate: Executed after I/O callback (readFile).");
+  });
+
+  console.log("File read successfully.");
+});
+
+Promise.resolve("Promise resolved.").then(console.log);
+
+setTimeout(() => console.log("setTimeout: Executed after main script."), 0);
+
+process.nextTick(() => console.log("NextTick: Executed after main script."));
+
+function sum(a, b) {
+  let res = a + b;
+  return res;
+}
+
+process.nextTick(() => console.log("NextTick: Scheduled with delay (should execute soon)."), 6000);
+
+const result = sum(a, b);
+
+console.log("Sum result:",result )
